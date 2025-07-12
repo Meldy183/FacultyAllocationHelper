6e62688b6a1b03083a4a5eac1353b2ef78276c9e
Shnupel <ufamail.com2@gmail.com>
2025-07-12 22:28:06 +0300

    micro fixes

--- shared/configs/constants/index.ts
+++ shared/configs/constants/index.ts
@@ -1,3 +1,5 @@
+import * as process from "node:process";
+
 export const facultyPositions = [
 	"Primary instructor",
 	"Tutor instructor",
@@ -5,3 +7,5 @@ export const facultyPositions = [
 ]
 
 export const API_PATH = process.env.NEXT_PUBLIC_BASE_API + "/api";
+
+export const DevMode = process.env.DEV_MODE
