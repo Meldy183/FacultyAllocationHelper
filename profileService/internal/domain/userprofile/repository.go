534dc81f4f62656c5bde112d8111569274ff33ed
ashuno <perekrestova.i@yandex.ru>
2025-07-16 13:45:43 +0300

    starting to fix domain layer

--- profileService/internal/domain/userprofile/repository.go
+++ profileService/internal/domain/userprofile/repository.go
@@ -6,5 +6,5 @@ type Repository interface {
 	AddProfile(ctx context.Context, profile *UserProfile) error
 	GetProfileByID(ctx context.Context, profileID int64) (*UserProfile, error)
 	UpdateProfileByID(ctx context.Context, profile *UserProfile) error
-	GetProfilesByFilter(ctx context.Context, institutes []int, positions []int) ([]int64, error)
+	// GetProfilesByFilter(ctx context.Context, institutes []int, positions []int) ([]int64, error)
 }
