742c3ab18cd62714053dd35b248c1d01ffc33fb0
Fyodor Markin <feduard2006@gmail.com>
2025-07-17 12:52:31 +0300

    interface for profile changed

--- profileService/internal/domain/facultyProfile/repository.go
+++ profileService/internal/domain/facultyProfile/repository.go
@@ -6,4 +6,6 @@ type Repository interface {
 	AddProfile(ctx context.Context, profile *UserProfile) error
 	GetProfileByID(ctx context.Context, profileID int64) (*UserProfile, error)
 	UpdateProfileByID(ctx context.Context, profile *UserProfile) error
+	GetProfileIDsByInstituteIDs(ctx context.Context, instituteIDs []int64) ([]int64, error)
+	GetProfileIDsByPositionIDs(ctx context.Context, positionIDs []int) ([]int64, error)
 }
