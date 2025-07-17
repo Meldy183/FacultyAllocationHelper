74674f096033b822fd2fe8d6448ebe05945b7219
Fyodor Markin <feduard2006@gmail.com>
2025-07-17 17:58:10 +0300

    model almost done

--- profileService/internal/handler/facultyProfile/model.go
+++ profileService/internal/handler/facultyProfile/model.go
@@ -1,5 +1,68 @@
 package facultyProfile
 
+type GetCourseListResponse struct {
+	Courses []Course `json:"courses"`
+}
+
+type AddNewCourseRequest struct {
+	BriefName              string `json:"brief_name"`
+	AcademicYearID         int    `json:"academic_year_id"`
+	SemesterID             int    `json:"semester_id"`
+	Year                   int    `json:"year"`
+	ProgramIDs             []int  `json:"program_ids"`
+	TrackIDs               []int  `json:"track_ids"`
+	ResponsibleInstituteID int    `json:"responsible_institute_id"`
+	GroupsNeeded           int    `json:"groups_needed"`
+	IsElective             bool   `json:"is_elective"`
+}
+
+type AddNewCourseResponse struct {
+	CourseID                 int      `json:"course_id"`
+	BriefName                string   `json:"brief_name"`
+	AcademicYearName         string   `json:"academic_year_name"`
+	SemesterName             string   `json:"semester_name"`
+	ProgramNames             []string `json:"program_names"`
+	TrackNames               []string `json:"track_names"`
+	ResponsibleInstituteName string   `json:"responsible_institute_name"`
+	GroupsNeeded             int      `json:"groups_needed"`
+	Pi                       string   `json:"pi"`  // empty
+	Ti                       string   `json:"ti"`  // empty
+	TAs                      []string `json:"tas"` // empty
+}
+
+type GetCourseResponse struct {
+	Course Course
+}
+
+type EditCourseRequest struct {
+	CourseID               int    `json:"course_id"`
+	BriefName              string `json:"brief_name"`
+	OfficialName           string `json:"official_name"`
+	ResponsibleInstituteID int    `json:"responsible_institute_id"`
+	StudyProgramIDS        []int  `json:"study_program_ids"`
+	TrackIDs               []int  `json:"track_ids"`
+	Mode                   string `json:"mode"`
+	StudyYearID            int    `json:"study_year_id"`
+	SemesterID             int    `json:"semester_id"`
+	Form                   string `json:"form"`
+	LectureHours           int    `json:"lecture_hours"`
+	LabHours               int    `json:"lab_hours"`
+}
+
+type EditCourseResponse struct {
+	CourseID                 int      `json:"course_id"`
+	BriefName                string   `json:"brief_name"`
+	OfficialName             string   `json:"official_name"`
+	ResponsibleInstituteName string   `json:"responsible_institute_name"`
+	StudyProgramNames        []string `json:"study_program_names"`
+	TrackNames               []string `json:"track_names"`
+	Mode                     string   `json:"mode"`
+	StudyYearName            string   `json:"study_year_name"`
+	SemesterName             string   `json:"semester_name"`
+	Form                     string   `json:"form"`
+	LectureHours             int      `json:"lecture_hours"`
+	LabHours                 int      `json:"lab_hours"`
+}
 type AddProfileRequest struct {
 	NameEnglish      string `json:"name_eng"`
 	Email            string `json:"email"`
