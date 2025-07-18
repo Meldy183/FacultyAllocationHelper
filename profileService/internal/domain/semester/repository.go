5b6fc3e28274cc324ff73256de9bff1df347019f
ashuno <perekrestova.i@yandex.ru>
2025-07-18 16:12:07 +0300

    interfaces done i think

--- profileService/internal/domain/semester/repository.go
+++ profileService/internal/domain/semester/repository.go
@@ -1,3 +1,7 @@
 package semester
 
-type Repository interface{}
+import "context"
+
+type Repository interface {
+	GetSemesterNameByID(ctx context.Context, semesterID int64) (*string, error)
+}
--- profileService/internal/domain/semesterWorkload/entity.go
+++ profileService/internal/domain/semesterWorkload/entity.go
@@ -2,23 +2,10 @@ package semesterworkload
 
 type SemesterWorkload struct {
 	SemesterWorkloadID int64
+	ProfileVersionID   int64
 	SemesterID         int
-	lecturesCount      int
-	tutorialsCount     int
-	labsCount          int
-	electivesCount     int
-}
-
-func NewSemesterWorkload(
-	semesterWorkloadID int64,
-	SemesterID, lecturesCount, tutorialsCount, labsCount, electivesCount int,
-) *SemesterWorkload {
-	return &SemesterWorkload{
-		SemesterWorkloadID: semesterWorkloadID,
-		SemesterID:         SemesterID,
-		lecturesCount:      lecturesCount,
-		tutorialsCount:     tutorialsCount,
-		labsCount:          labsCount,
-		electivesCount:     electivesCount,
-	}
+	LecturesCount      int
+	TutorialsCount     int
+	LabsCount          int
+	ElectivesCount     int
 }
