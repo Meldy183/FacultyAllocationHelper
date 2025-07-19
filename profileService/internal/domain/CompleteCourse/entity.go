26cce2cad2cfcb0a640a08682473230b7c91dfe0
Fyodor Markin <feduard2006@gmail.com>
2025-07-19 14:53:54 +0300

    FullCourse service

---
+++ profileService/internal/domain/CompleteCourse/entity.go
@@ -0,0 +1,13 @@
+package CompleteCourse
+
+import (
+	"gitlab.pg.innopolis.university/f.markin/fah/profileService/internal/domain/course"
+	"gitlab.pg.innopolis.university/f.markin/fah/profileService/internal/domain/courseInstance"
+)
+
+type FullCourse struct {
+	course.Course
+	courseInstance.CourseInstance
+	StudyPrograms []int
+	Tracks        []int
+}
--- profileService/internal/domain/course/entity.go
+++ profileService/internal/domain/course/entity.go
@@ -8,19 +8,3 @@ type Course struct {
 	LecHours               *int
 	LabHours               *int
 }
-
-func NewCourse(
-	courseID, responsibleInstituteID int64,
-	name string,
-	offName *string,
-	lecHours, labHours *int,
-) (*Course, error) {
-	return &Course{
-		CourseID:               courseID,
-		Name:                   name,
-		OfficialName:           offName,
-		ResponsibleInstituteID: responsibleInstituteID,
-		LecHours:               lecHours,
-		LabHours:               labHours,
-	}, nil
-}
