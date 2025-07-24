336fa4db311681cb9c91da475bbe76e95022b503
feduard <feduard2006@gmail.com>
2025-07-24 23:08:14 +0300

    evening work end

--- profileService/cmd/profile-service/main.go
+++ profileService/cmd/profile-service/main.go
@@ -168,7 +168,7 @@ func main() {
 
 func initLogger() (*zap.Logger, error) {
 	cfg := zap.NewProductionConfig()
-	cfg.OutputPaths = []string{"stdout"} // redirect logs to stdout
+	cfg.OutputPaths = []string{"stdout"}
 	logger, err := cfg.Build()
 	if err != nil {
 		return nil, err
