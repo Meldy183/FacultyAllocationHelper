427f9cfebbc7ecdc6bcbb9148dde690515bd1755
Gnida <ivankusaka004@gmail.com>
2025-07-27 11:36:50 +0300

    interfaces for small entities updated

--- profileService/internal/domain/program/repository.go
+++ profileService/internal/domain/program/repository.go
@@ -5,4 +5,5 @@ import "context"
 type Repository interface {
 	GetAllPrograms(ctx context.Context) ([]*Program, error)
 	GetProgramNameByID(ctx context.Context, id int64) (*string, error)
+	GetProgramIDByName(ctx context.Context, name string) (*int64, error)
 }
--- profileService/internal/domain/responsibleInstitute/repository.go
+++ profileService/internal/domain/responsibleInstitute/repository.go
@@ -5,4 +5,5 @@ import "context"
 type Repository interface {
 	GetResponsibleInstituteNameByID(ctx context.Context, responsibleInstituteID int64) (*string, error)
 	GetAllInstitutes(ctx context.Context) ([]ResponsibleInstitute, error)
+	GetResponsibleInstituteIDByName(ctx context.Context, responsubleInstituteName string) (*int64, error)
 }
