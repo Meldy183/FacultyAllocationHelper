f6dc63afc6afaa036f76b1a3d5e80861ad1beb68
pavel <balandinpavel897@gmail.com>
2025-08-04 23:40:06 +0300

    migrate to FSD logs page

--- app/(pages)/logs/page.tsx
+++ app/(pages)/logs/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Wrapper from "@/shared/ui/wrapper";
+import { LogRecord } from "./modules/LogRecors";
 import styles from "./styles.module.scss";
 
 const LogsPage: React.FC = () => {
@@ -24,23 +25,6 @@ const LogsPage: React.FC = () => {
             <LogRecord/>
         </ul>
 	</Wrapper>
-}
-
-
-const LogRecord: React.FC = () => {
-	return (
-		<li className={styles.row}>
-			<div className={styles.colName}>
-                <h2>Name Surname</h2>
-                <div>@alias</div>
-			</div>
-			<div className={styles.colInstitute}>
-                <h4>02.07.2025</h4>
-                <h4>17:35</h4>
-            </div>
-			<div className={styles.colEmail}>Did something</div>
-		</li>
-	)
 }
 
 
